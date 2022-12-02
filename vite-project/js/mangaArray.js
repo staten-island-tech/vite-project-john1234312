23c886b00e87fb4f1678c3c14965449936d3ac5d
john1234312 <110704261+john1234312@users.noreply.github.com>
2022-12-02 14:00:07 -0500

    ds

---
+++ vite-project/js/mangaArray.js
@@ -0,0 +1,43 @@
+manga: [
+{
+    name: "Ayashimon",
+    author: "Yuji Kaku",
+    dateReleased: "11-2021",
+    genre: ["Action", "Sci-Fi", "Horror"]
+},
+{
+    name: "Chainsaw Man",
+    author: "Tatsuki Fujimoto",
+    dateReleased: "12-2018",
+    genre: ["Action", "Horror"]
+}
+{
+    name:"Kaiju no.8",
+    author:"Naoya Matsumoto",
+    dateReleased:"07-2020",
+    genre: ["Adventure", "Sci-Fi"]
+}
+{
+    name:"Monster",
+    author:"Noaki Urasawa",
+    dateReleased:"12-1994",
+    genre:["Crime", "Mystery", "Psychological Thriller"]
+}
+{
+    name:"Grappler Baki",
+    author:"Keisuku Itagaki",
+    dateReleased:"10-1991",
+    genre:["Action"]
+}
+{
+    name:,
+    author:,
+    dateReleased:,
+    genre:
+}
+{
+    name:,
+    author:,
+    dateReleased:,
+    genre:
+}]
